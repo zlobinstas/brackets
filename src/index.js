68457a9920528804eaaef59623b25c0b9fb3c936
zlobinstas <zlobinstas@bk.ru>
2021-03-03 19:57:18 +0100

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[0] == 1) {
+            return true;
+        }
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[' || str[i] == "|" && result[result.length - 1] != "|") {
+            result.push(str[i]);
+        } else if (result[result.length - 1] == '(' && str[i] == ")" || result[result.length - 1] == '{' && str[i] == "}" || result[result.length - 1] == '[' && str[i] == "]" || str[i] == "|" && result[result.length - 1] == "|") {
+            result.pop();
+        } else {
+            return false;
+        }
+    } return result[0] == undefined ? true : false;
+
 }
